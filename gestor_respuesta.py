f8e622ee638b3ad1e14c57c0b04af16e523070fc
Arnau Campos <arnauc6@gmail.com>
2017-05-23 11:34:50 +0200

    Configuración variables Heroku

--- gestor_respuesta.py
+++ gestor_respuesta.py
@@ -6,6 +6,7 @@
 ##------------------------------------------------------------------------------
 
 import pymongo
+import os
 
 # Importamos todos los módulos que contienen las respuestas a las distintas preguntas
 from respuestas.pago_barrios import *
@@ -23,7 +24,13 @@ except:
     pass
 #------------------------------ Eliminar si añadimos la uri en el campo inferior
 
-uri = tu_URL_de_MongoDB # Sustituir tu_URL_de_MongoDB por la URL de tu servidor
+##uri = tu_URL_de_MongoDB # Sustituir tu_URL_de_MongoDB por la URL de tu servidor
+try:
+    uri = os.environ['URL_de_MongoDB'] # Variable de entorno para Heroku
+except:
+##    uri = 'mongodb://localhost:27017/collection'
+    print "Error al cargar URL de MongoDB"
+
 client = pymongo.MongoClient(uri)
 db = client.get_default_database()
 dbValencia = db.impuestosBarrios # Accedemos a la colección donde almacenamos todos los datos
