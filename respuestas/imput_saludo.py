7e8d7f35070aa60776b3409f301b291d54d4f9e3
Arnau Campos <arnauc6@gmail.com>
2017-05-25 12:49:28 +0200

    Respuesta a saludo

--- respuestas/imput_saludo.py
+++ respuestas/imput_saludo.py
@@ -16,7 +16,7 @@ from unir_texto import *
 #   - El barrio de Benimaclet paga 6,428,288.48 € de impuestos
 # Texto en la lista:
 ##textoRespuesta = [u"El barrio de ", u" paga ", u"€ de "]
-textoRespuesta = "Buenos días"
+textoRespuesta = u"Buenos días"
 
 
 ##//////////////////////////////////////////////////////////////////////////////
